ba26f8b2427ad4b905cdef6bf94adecf229558de
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2025-01-29 22:49:45 +0900

    [#6681bb6] UPDATE: GITHUB PAGES

---
+++ docs/javascripts/custom.js
@@ -0,0 +1,62 @@
+var _0x34472a = _0x2fea;
+function _0xfebf() {
+  var _0x3a7f6c = [
+    "14ltwzDU",
+    "63rDKLhz",
+    "addEventListener",
+    "1993046lhzNPJ",
+    "1340916hoLEjj",
+    "766268DyXJQi",
+    "2980520isooUS",
+    "335390zcRnMc",
+    "613230NJUbYw",
+    "preventDefault",
+    "367459issKtq",
+    "selectstart",
+    "2KNmTRN",
+    "5vDgSqu",
+  ];
+  _0xfebf = function () {
+    return _0x3a7f6c;
+  };
+  return _0xfebf();
+}
+function _0x2fea(_0x20f70d, _0x11e0ab) {
+  var _0xfebf61 = _0xfebf();
+  return (
+    (_0x2fea = function (_0x2fea0b, _0x3babe9) {
+      _0x2fea0b = _0x2fea0b - 0x14d;
+      var _0x1c5263 = _0xfebf61[_0x2fea0b];
+      return _0x1c5263;
+    }),
+    _0x2fea(_0x20f70d, _0x11e0ab)
+  );
+}
+(function (_0x36e632, _0x1b927f) {
+  var _0x3f9867 = _0x2fea,
+    _0x3fe463 = _0x36e632();
+  while (!![]) {
+    try {
+      var _0x50a1f1 =
+        -parseInt(_0x3f9867(0x14d)) / 0x1 +
+        (parseInt(_0x3f9867(0x14f)) / 0x2) *
+          (-parseInt(_0x3f9867(0x155)) / 0x3) +
+        (-parseInt(_0x3f9867(0x156)) / 0x4) *
+          (-parseInt(_0x3f9867(0x150)) / 0x5) +
+        (parseInt(_0x3f9867(0x159)) / 0x6) *
+          (parseInt(_0x3f9867(0x151)) / 0x7) +
+        parseInt(_0x3f9867(0x157)) / 0x8 +
+        (parseInt(_0x3f9867(0x152)) / 0x9) *
+          (parseInt(_0x3f9867(0x158)) / 0xa) +
+        parseInt(_0x3f9867(0x154)) / 0xb;
+      if (_0x50a1f1 === _0x1b927f) break;
+      else _0x3fe463["push"](_0x3fe463["shift"]());
+    } catch (_0x4dc4dd) {
+      _0x3fe463["push"](_0x3fe463["shift"]());
+    }
+  }
+})(_0xfebf, 0x5a596),
+  document[_0x34472a(0x153)](_0x34472a(0x14e), function (_0x21e1bd) {
+    var _0x26bdc6 = _0x34472a;
+    _0x21e1bd[_0x26bdc6(0x15a)]();
+  });
